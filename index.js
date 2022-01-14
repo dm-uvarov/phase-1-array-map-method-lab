c5f2daf5e85d8e2f55dde84c819fe61fb65aba7f
Jimmy <dm.a.uvarov@gmail.com>
2022-01-14 14:27:21 -0500

    commit tree 04f8527c3c4c0763135e065b9b99e66038010058

--- index.js
+++ index.js
@@ -12,5 +12,63 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return mapMap(tutorials)
 }
+
+
+function mapMap (arr){
+    return arr.map ((t) => {
+     return splitMapNjoin(t)
+   })
+
+  }
+
+
+
+
+// console.log(titleCased);
+// console.log(mapMap(tutorials));
+// works as well
+// const splitIt = s => s.split(" ");
+
+// works
+// function splitIt (s) {
+//   return s.split(" ");
+  
+// }
+
+
+function splitMapNjoin(s) { 
+  return change1Char(s.split(" ")).join(" ")
+}
+
+// console.log(splitIt("wy das myhart feelso"));
+
+
+// console.log(splitMapNjoin("wy das myhart feelso"));
+// splitIt("wy das myhart feelso");
+
+//const mappedArr = arr.map((s) => s.toUppercase);
+
+
+// // works
+// function mapIt(arr) {
+//   return arr.map((s) => s.toUpperCase())
+// }
+
+// // works fine
+// function Uppercase1stChar(s) {
+//   return s.substring(0,1).toUpperCase() + s.substring(1)
+// }
+
+//works as well 
+function change1Char(arr) {
+  return arr.map((s) => {
+    return s.substring(0,1).toUpperCase() + s.substring(1);
+  })
+}
+
+
+//onsole.log(Uppercase1stChar("zabaza"));
+// //console.log(mapIt(tutorials));
+// console.log(change1Char(tutorials).join(" "));
